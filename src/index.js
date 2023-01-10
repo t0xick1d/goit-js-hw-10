3ebdecfeb7f760e8649ee27f85e607db7e8f577c
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-01-10 19:21:34 +0200

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 
+var debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
+
+const inputElement = document.querySelector('#search-box');
+const listCountriesriElement = document.querySelector('.country-list');
+const currendFindCountryCard = document.querySelector('.country-info');
+
+inputElement.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  const currentValueInput = e.target.value;
+
+  fetch(`https://restcountries.com/v3.1/name/${currentValueInput}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (data.length <= 10 && data.length >= 2) {
+        cleanSearch();
+        MarkUpList(data);
+        return;
+      }
+      if (data.length === 1) {
+        cleanSearch();
+        MarkUpActualCountry(data);
+        return;
+      }
+      if (data.status === 404 && data.message === 'Not Found') {
+        cleanSearch();
+        Notify.failure('Oops, there is no country with that name');
+      }
+    });
+}
+
+function cleanSearch() {
+  listCountriesriElement.innerHTML = '';
+  currendFindCountryCard.innerHTML = '';
+}
+function MarkUpList(data) {
+  listCountriesriElement.innerHTML = data
+    .map(({ flags, name }) => {
+      return `<li>
+            <img src=${flags.svg} width='50'/>
+            ${name.common}
+            </li>`;
+    })
+    .join(' ');
+}
+function MarkUpActualCountry(data) {
+  currendFindCountryCard.innerHTML = data
+    .map(element => {
+      const langs = Object.values(element.languages).join(', ');
+      const capitals = element.capital.join(', ');
+      return `
+              <div><img src=${element.flags.svg} width='40'/> ${element.name.common}</div>
+              <div>Capital: ${capitals}</div>
+              <div>Population: ${element.population}</div>
+              <div>Language: ${langs}</div>
+            `;
+    })
+    .join(' ');
+}
